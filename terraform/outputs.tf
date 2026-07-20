5caf7b2e7e5d13a0338feb2fdf79c69348277d2e
Taha Demir <taha@Tahas-MacBook-Pro.local>
2026-07-20 21:24:37 +0300

    Expand Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "bucket_name" {
 output "bucket_arn" {
   value = aws_s3_bucket.data_lake.arn
 }
+
+output "bucket_region" {
+  value = aws_s3_bucket.data_lake.region
+}
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.data_lake.bucket_domain_name
+}
