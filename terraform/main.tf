57e261aac459e9f198ba3afb09b5978ca792c69b
Taha Demir <taha@Tahas-MacBook-Pro.local>
2026-07-21 09:35:58 +0300

    Centralize resource tags using Terraform locals

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,6 @@ resource "aws_s3_bucket" "data_lake" {
 
   bucket = var.bucket_name
 
-  tags = {
-    Project     = "Modern Data Platform"
-    Environment = "Learning"
-    ManagedBy   = "Terraform"
-  }
+  tags = local.common_tags
 
 }
