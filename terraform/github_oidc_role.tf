9d55981cc5afaa3403baa515efa8295b4116e3a3
Taha Yasin Demir <thydemir@gmail.com>
2026-08-02 22:27:08 +0300

    Document GitHub OIDC trust policy

--- terraform/github_oidc_role.tf
+++ terraform/github_oidc_role.tf
@@ -19,7 +19,11 @@ data "aws_iam_policy_document" "github_oidc_assume_role" {
         "sts.amazonaws.com"
       ]
     }
-
+    
+    # GitHub repositories created after July 15, 2026 use immutable OIDC
+    # subject claims that include owner and repository identifiers.
+    # This trust policy remains scoped to this repository while allowing
+    # those immutable identifiers to vary.
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
